9963be450a281c744ff9465c1d16503689669256
S-kaitei <oren.hagetaka@gmail.com>
2021-09-11 21:49:26 +0900

    add:csvExport

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Illuminate\Http\Request;
 use App\Contact;
+use PhpParser\Node\Expr\AssignOp\Concat;
 
 class ContactController extends Controller
 {
@@ -17,5 +19,38 @@ class ContactController extends Controller
             ->get();
 
         return view('index',compact('contacts'));
+    }
+
+    public function csvExport(Request $request)
+    {
+        $post = $request->all();
+        $response = new StreamedResponse(function () use ($request, $post) {
+
+            $stream = fopen('php://output','w');
+            $contact = new Contact();
+
+            // 文字化け回避
+            stream_filter_prepend($stream, 'convert.iconv.utf-8/cp932//TRANSLIT');
+
+            // ヘッダー行を追加
+            fputcsv($stream, $contact->csvHeader());
+
+            $results = $contact->getCsvData($post['start_date'], $post['end_date']);
+
+            if(empty($results[0])) {
+                fputcsv($stream, [
+                    'データが存在しませんでした。',
+                ]);
+            } else {
+                foreach ($results as $row) {
+                    fputcsv($stream, $contact->csvRow($row));
+                }
+            }
+            fclose($stream);
+        });
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('content-disposition', 'attachment; filename='. $post['start_date'] . '〜' . $post['end_date'] . 'お問い合わせ一覧.csv');
+
+        return $response;
     }
 }
